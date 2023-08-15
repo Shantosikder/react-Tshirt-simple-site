4b27ffd8c842a4a45a7d580834e24d6889afd328
Shantosikder <shantosikder77@gmail.com>
2023-08-15 14:38:00 +0600

    product show and order summary add

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,14 +1,35 @@
-import React from 'react';
-import { useLoaderData } from 'react-router-dom';
+import React, { useState } from "react";
+import { useLoaderData } from "react-router-dom";
+import TShirt from "../TShirt/TShirt";
+import Cart from "../Cart/Cart";
+import "./Home.css";
 
 const Home = () => {
-    const tshirts = useLoaderData();
-    // console.log(tshirts);
-    return (
-        <div>
-            <h1>This is home page : {tshirts.length}</h1>
-        </div>
-    );
+  const tshirts = useLoaderData();
+  // console.log(tshirts);
+  const [cart, setCart] = useState([]);
+
+  const handleAddToCart = (tshirt) => {
+    // console.log(tshirt);
+    const newCart = [...cart, tshirt];
+    setCart(newCart);
+  };
+  return (
+    <div className="home-container">
+      <div className="t-shirt-container">
+        {tshirts.map((tshirt) => (
+          <TShirt
+            key={tshirt._id}
+            tshirt={tshirt}
+            handleAddToCart={handleAddToCart}
+          ></TShirt>
+        ))}
+      </div>
+      <div className="cart-container">
+        <Cart cart={cart}></Cart>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
